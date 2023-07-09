37b802454e908841ab45a756119b21e29b116b15
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-09 17:04:08 -0300

    Create the skelet of user service

--- src/services/port/user.service.port.ts
+++ src/services/port/user.service.port.ts
@@ -1,5 +1,10 @@
 import { Either } from "../../shared/errors/either";
 
 export interface UserSerivcePort {
-  create: () => Promise<Either<Error, unknown>>
+  create: (input: unknown) => Promise<Either<Error, unknown>>
+  update: (input: unknown) => Promise<Either<Error, unknown>>
+  delete: (id: string) => Promise<Either<Error, unknown>>
+  findById: (id: string) => Promise<Either<Error, unknown>>
+  findAll: () => Promise<Either<Error, unknown>>
+  validate: () => Promise<Either<Error, unknown>>
 }
