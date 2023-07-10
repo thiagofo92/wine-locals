331a2ad3c81552d341b9be1d7686425a3644b732
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-10 15:23:48 -0300

    Return error user not found when findbyid not found the user ;

--- src/infra/services/memory/user.memory.service.ts
+++ src/infra/services/memory/user.memory.service.ts
@@ -27,28 +27,40 @@ export class UserMemoryService implements UserSerivcePort {
   }
   async delete (id: string):  Promise<Either<Error, boolean>> {
     try {
-      return right('') 
+      const index = this.users.findIndex(item => item.id === id)
+
+      if(index < 0) return left(new UserServiceUserNotFound())
+
+      this.users.splice(index, 1)
+
+      return right(true) 
     } catch (error: any) {
       return left(error) 
     }
   }
-  async findById (id: string): Promise<Either<Error, UserEntity>>{
+  async findById (id: string): Promise<Either<Error, UserEntity | null>>{
     try {
-      return right('') 
+      const user = this.users.find(item => item.id === id)
+
+      if(!user) return left(new UserServiceUserNotFound())
+      
+      return right(user) 
     } catch (error: any) {
       return left(error) 
     }
   }
   async findAll (): Promise<Either<Error, UserEntity[]>> {
     try {
-      return right('') 
+      const users = this.users
+      return right(users) 
     } catch (error: any) {
       return left(error) 
     }
   }
-  async validate (): Promise<Either<Error, boolean>> {
+  async validate (email: string, password: string): Promise<Either<Error, boolean>> {
     try {
-      return right('') 
+      const user = this.users.find(item => item.email === email && item.password === password)
+      return right(user ? true : false) 
     } catch (error: any) {
       return left(error) 
     }
