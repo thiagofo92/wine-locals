0443d547f5cd5e395dd94a51f5f079b33d0333d7
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-10 10:35:27 -0300

    Add data struct to user entity

--- src/core/entities/user.entity.ts
+++ src/core/entities/user.entity.ts
@@ -1,3 +1,33 @@
-export class UserEntity {
+import { randomUUID } from "crypto"
 
+interface UserData {
+  id?: string,
+  name: string,
+  cpf: string,
+  birthday: string,
+  password: string,
+  email: string
+}
+export class UserEntity {
+  id: string
+  name: string
+  cpf: string
+  birthday: string
+  password: string
+  email: string
+
+  constructor(user: UserData) {
+    this.id = user.id ? user.id : randomUUID()
+    this.name = user.name
+    this.cpf = user.cpf
+    this.birthday = user.birthday
+    this.password = user.password
+    this.email = user.email
+  }
+
+  hasLegalAge(): boolean {
+    const birthday = new Date(this.birthday)
+    const now = new Date()
+    return (now.getFullYear() - birthday.getFullYear()) >= 18
+  }
 }
