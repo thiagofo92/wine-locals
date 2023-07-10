ef3d805f5cddc2e64fde531bf2a1e5d89d08bbc3
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-10 10:37:56 -0300

    Initial setup of express api

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -1,16 +1,16 @@
 import Express from 'express'
 import { RoutersServer } from './routers/router'
 
-
-const app = Express()
+export const app = Express()
 
 const port = process.env.SERVER_PORT || '3000'
 const host = process.env.host || '0.0.0.0'
 
-const router = new RoutersServer(Express.Router())
+export function StartServer() {
+  const router = new RoutersServer(Express.Router())
 
-app.use(router.build())
-
-app.listen(Number(port), host, () => {
-  console.log(`Server is running ${host}:${port}`)
-})
+  app.use(router.build())
+  app.listen(Number(port), host, () => {
+    console.log(`Server is running ${host}:${port}`)
+  })
+}
---
+++ src/main/index.ts
@@ -0,0 +1,3 @@
+import { StartServer } from "./api";
+
+StartServer()
