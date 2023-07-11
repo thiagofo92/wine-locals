d3d0c7d06517fc6a91954ebd020a104507980cdc
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-11 16:07:00 -0300

    Change value of return type

--- src/infra/services/port/event.service.port.ts
+++ src/infra/services/port/event.service.port.ts
@@ -1,7 +1,7 @@
-import { type WineTourismEntity } from '@/core/entities'
+import { type EventEntity } from '@/core/entities'
 import { type Either } from '@/shared/errors/either'
 
 export interface EventSerivcePort {
-  create: (input: WineTourismEntity) => Promise<Either<Error, boolean>>
-  findAll: () => Promise<Either<Error, WineTourismEntity[]>>
+  create: (input: EventEntity) => Promise<Either<Error, boolean>>
+  findAll: () => Promise<Either<Error, EventEntity[]>>
 }
