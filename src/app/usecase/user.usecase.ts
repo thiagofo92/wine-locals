3e5ebf3f823dc59d0c1803b39d20c91a7d3ea881
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-11 16:09:51 -0300

    FIx eslint format

--- src/app/usecase/user.usecase.ts
+++ src/app/usecase/user.usecase.ts
@@ -1,42 +1,44 @@
-import { Either, left, right } from "@/shared/errors/either";
-import { UserUseCasePort } from "../port";
-import { UserSerivcePort } from "@/infra/services/port";
-import { UserAppDto } from "../dto";
-import { UserEntity } from "@/core/entities";
-import { UserUseCaseLegalAgeError } from "../errors";
+import { type Either, left, right } from '@/shared/errors/either'
+import { type UserUseCasePort } from '../port'
+import { type UserSerivcePort } from '@/infra/services/port'
+import { type UserAppDto } from '../dto'
+import { UserEntity } from '@/core/entities'
+import { UserUseCaseLegalAgeError } from '../errors'
 
 export class UserUseCase implements UserUseCasePort {
-  constructor(private readonly service: UserSerivcePort) {}
-  
-  async create (input: UserAppDto): Promise<Either<Error, string>>{
+  constructor (private readonly service: UserSerivcePort) {}
+
+  async create (input: UserAppDto): Promise<Either<Error, string>> {
     const user = new UserEntity(input)
 
-    if(!user.hasLegalAge()) return left(new UserUseCaseLegalAgeError())
+    if (!user.hasLegalAge()) return left(new UserUseCaseLegalAgeError())
 
     const result = await this.service.create(user)
 
-    if(result.isLeft()) {
+    if (result.isLeft()) {
       return left(result.value)
     }
 
     return right(result.value)
   }
+
   async validate (email: string, password: string): Promise<Either<Error, boolean>> {
-   const result = await this.service.validate(email, password) 
+    const result = await this.service.validate(email, password)
 
-   if(result.isLeft()) {
-    return left(new Error())
-   }
+    if (result.isLeft()) {
+      return left(new Error())
+    }
 
-   return right(result.value)
+    return right(result.value)
   }
-  async findById (id: string): Promise<Either<Error, UserAppDto>>{
+
+  async findById (id: string): Promise<Either<Error, UserAppDto>> {
     const result = await this.service.findById(id)
-   
-    if(result.isLeft()) {
+
+    if (result.isLeft()) {
       return left(result.value)
     }
-    
+
     const { value } = result
     const user: UserAppDto = {
       id: value!.id,
@@ -49,5 +51,4 @@ export class UserUseCase implements UserUseCasePort {
 
     return right(user)
   }
-
 }
