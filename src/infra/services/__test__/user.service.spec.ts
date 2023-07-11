f8239945f8948dd5c39f7b92ad111203c1b7f87a
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-11 15:45:30 -0300

    Update

--- src/infra/services/__test__/user.service.spec.ts
+++ src/infra/services/__test__/user.service.spec.ts
@@ -1,12 +1,12 @@
 import { describe, expect, test, vi } from 'vitest'
 import { UserMemoryService } from '../memory/user.memory.service'
-import { UserMock } from '../__mocks__/user.mock'
+import { UserMock } from '../__mocks__/user.service.mock'
 import { left } from '@/shared/errors/either'
-import { UserServiceUserNotFound } from '../errors/user.service.error'
-import { UserEntity } from '@/core/entities'
+import { DataServiceNotFound } from '../errors/data.service.error'
+import { type UserEntity } from '@/core/entities'
 
-describe("# User service", () => {
-  test("Register user to database", async () => {
+describe('# User service', () => {
+  test('Register user to database', async () => {
     const service = new UserMemoryService()
     const user = UserMock
     const result = await service.create(user)
@@ -14,7 +14,7 @@ describe("# User service", () => {
     expect(result.value).toStrictEqual(user.id)
   })
 
-  test("Fail to register user", async () => {
+  test('Fail to register user', async () => {
     const service = new UserMemoryService()
     const user = UserMock
     vi.spyOn(service, 'create').mockResolvedValueOnce(left(new Error()))
@@ -23,30 +23,30 @@ describe("# User service", () => {
     expect(result.value).toBeInstanceOf(Error)
   })
 
-  test("Update user", async () => {
+  test('Update user', async () => {
     const service = new UserMemoryService()
     const user = UserMock
     await service.create(user)
-    user.name = "Test"
+    user.name = 'Test'
     user.email = 'test@test.com.br'
 
     const result = await service.update(user)
     expect(result.value).toStrictEqual(true)
   })
 
-  test("Not found user to update", async () => {
+  test('Not found user to update', async () => {
     const service = new UserMemoryService()
     const user = UserMock
     await service.create(user)
-    user.name = "Test"
+    user.name = 'Test'
     user.email = 'test@test.com.br'
 
-    vi.spyOn(service, 'update').mockResolvedValueOnce(left(new UserServiceUserNotFound()))
+    vi.spyOn(service, 'update').mockResolvedValueOnce(left(new DataServiceNotFound()))
     const result = await service.update(user)
-    expect(result.value).toBeInstanceOf(UserServiceUserNotFound)
+    expect(result.value).toBeInstanceOf(DataServiceNotFound)
   })
 
-  test("Delete user", async () => {
+  test('Delete user', async () => {
     const service = new UserMemoryService()
     const user = UserMock
 
@@ -56,19 +56,19 @@ describe("# User service", () => {
     expect(result.value).toStrictEqual(true)
   })
 
-  test("User not found when try to delete", async () => {
+  test('User not found when try to delete', async () => {
     const service = new UserMemoryService()
     const user = UserMock
 
-    vi.spyOn(service, 'delete').mockResolvedValueOnce(left(new UserServiceUserNotFound()))
+    vi.spyOn(service, 'delete').mockResolvedValueOnce(left(new DataServiceNotFound()))
 
     await service.create(user)
     const result = await service.delete(user.id)
 
-    expect(result.value).toBeInstanceOf(UserServiceUserNotFound)
+    expect(result.value).toBeInstanceOf(DataServiceNotFound)
   })
 
-  test("Find user by id", async () => {
+  test('Find user by id', async () => {
     const service = new UserMemoryService()
     const user = UserMock
 
@@ -78,24 +78,24 @@ describe("# User service", () => {
     expect(result.value).toStrictEqual(user)
   })
 
-  test("User not found using ID", async () => {
+  test('User not found using ID', async () => {
     const service = new UserMemoryService()
 
     const result = await service.findById('asf')
-    expect(result.value).toBeInstanceOf(UserServiceUserNotFound)
+    expect(result.value).toBeInstanceOf(DataServiceNotFound)
   })
 
-  test("Find all user", async () => {
+  test('Find all user', async () => {
     const service = new UserMemoryService()
     const user = UserMock
-    
+
     await service.create(user)
     const result = await service.findAll()
     const value = result.value as UserEntity []
     expect(value.length).toBeGreaterThan(0)
   })
 
-  test("Valide the email and password", async () => {
+  test('Valide the email and password', async () => {
     const service = new UserMemoryService()
     const user = UserMock
 
