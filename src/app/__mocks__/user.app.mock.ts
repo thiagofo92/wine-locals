38c2113eed3fde4623bf11daa489e9ee7e8729a2
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-11 16:24:04 -0300

    Fix eslint format

--- src/app/__mocks__/user.app.mock.ts
+++ src/app/__mocks__/user.app.mock.ts
@@ -1,12 +1,12 @@
-import { faker } from "@faker-js/faker";
-import { UserAppDto } from "../dto";
-import { randomUUID } from "crypto";
+import { faker } from '@faker-js/faker'
+import { type UserAppDto } from '../dto'
+import { randomUUID } from 'crypto'
 
 export const UserAppMock: UserAppDto = {
   id: randomUUID(),
   name: faker.person.fullName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
-  cpf: faker.string.numeric({ length: 11}),
+  cpf: faker.string.numeric({ length: 11 }),
   birthday: faker.date.birthdate().toUTCString()
 }
