476a7779598284a58b7c0ad33d2e7bb3cf15cebb
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-11 18:35:25 -0300

    Created the port for usecase winery

---
+++ src/app/port/user.usecase.port.ts
@@ -0,0 +1,8 @@
+import { type Either } from '@/shared/errors/either'
+import { type UserAppDto } from '../dto'
+
+export interface UserUseCasePort {
+  create: (input: UserAppDto) => Promise<Either<Error, string>>
+  validate: (email: string, password: string) => Promise<Either<Error, boolean>>
+  findById: (id: string) => Promise<Either<Error, UserAppDto>>
+}
