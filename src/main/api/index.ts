596a203c048d1b982c6993183e953debf059096c
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-13 16:04:10 -0300

    Add json to middleware of express

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -1,6 +1,7 @@
 import Express from 'express'
 import { RoutersServer } from './routers/router'
-
+import { serve as SwaggerServer, setup as SwaggerSetUp } from 'swagger-ui-express'
+import SwaggerConfig from './middlewares/swagger/swagger-config.json'
 export const app = Express()
 
 const port = process.env.SERVER_PORT || '3000'
@@ -8,7 +9,8 @@ const host = process.env.host || '0.0.0.0'
 
 export function StartServer (): void {
   const router = new RoutersServer(Express.Router())
-
+  app.use(Express.json())
+  app.use('/api-docs', SwaggerServer, SwaggerSetUp(SwaggerConfig))
   app.use('/api', router.build())
   app.listen(Number(port), host, () => {
     console.log(`Server is running ${host}:${port}`)
