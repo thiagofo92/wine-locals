c352ac108692a70f21782bfd31980e463ab0dc15
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-13 16:04:40 -0300

    Created the routers with controller and middleware

--- src/main/api/routers/router.ts
+++ src/main/api/routers/router.ts
@@ -3,39 +3,44 @@ import { type Router } from 'express'
 import { userFactoryController } from './factory-controller/users.factory.controller'
 import { wineTourismFactoryController, wineryFactoryController } from './factory-controller'
 import { eventFactoryController } from './factory-controller/event.factory.controller'
+import { UserCreateMiddleware, UserValidateMiddleware, UserByIdMiddleware } from '../middlewares/schema/user.schema'
+import {
+  WineTourismByIdMiddleware, EventCreateMiddleware, WineTourismUpdateMiddleware,
+  WineryByIdMiddleware, WineryCreateMiddleware, WineryUpdateMiddleware
+} from '../middlewares/schema'
 
 export class RoutersServer {
   constructor (private readonly router: Router) {}
 
   private users (): void {
     const controller = userFactoryController()
-    this.router.post('/users', controller.create.bind(controller))
-    this.router.get('/users/id', controller.findById.bind(controller))
-    this.router.post('/users/validate', controller.validate.bind(controller))
+    this.router.post('/users', UserCreateMiddleware, controller.create.bind(controller))
+    this.router.get('/users', UserByIdMiddleware, controller.findById.bind(controller))
+    this.router.post('/users/validate', UserValidateMiddleware, controller.validate.bind(controller))
   }
 
   private winery (): void {
     const controller = wineryFactoryController()
-    this.router.post('/winery', controller.create.bind(controller))
-    this.router.put('/winery', controller.update.bind(controller))
-    this.router.get('/winery/id', controller.findById.bind(controller))
-    this.router.delete('/winery/id', controller.delete.bind(controller))
-    this.router.get('/winery', controller.findAll.bind(controller))
+    this.router.post('/winery', WineryCreateMiddleware, controller.create.bind(controller))
+    this.router.put('/winery', WineryUpdateMiddleware, controller.update.bind(controller))
+    this.router.get('/winery', WineryByIdMiddleware, controller.findById.bind(controller))
+    this.router.delete('/winery', WineryByIdMiddleware, controller.delete.bind(controller))
+    this.router.get('/winery/list', controller.findAll.bind(controller))
   }
 
   private wineTourism (): void {
     const controller = wineTourismFactoryController()
-    this.router.post('/winery', controller.create.bind(controller))
-    this.router.put('/winery', controller.update.bind(controller))
-    this.router.get('/winery/id', controller.findById.bind(controller))
-    this.router.delete('/winery/id', controller.delete.bind(controller))
-    this.router.get('/winery', controller.findAll.bind(controller))
+    this.router.post('/wine-tourism', EventCreateMiddleware, controller.create.bind(controller))
+    this.router.put('/wine-tourism', WineTourismUpdateMiddleware, controller.update.bind(controller))
+    this.router.get('/wine-tourism', WineTourismByIdMiddleware, controller.findById.bind(controller))
+    this.router.delete('/wine-tourism', WineTourismByIdMiddleware, controller.delete.bind(controller))
+    this.router.get('/wine-tourism/list', controller.findAll.bind(controller))
   }
 
   private event (): void {
     const controller = eventFactoryController()
-    this.router.post('/event', controller.create)
-    this.router.get('/event', controller.findAll)
+    this.router.post('/events', EventCreateMiddleware, controller.create.bind(controller))
+    this.router.get('/events/list', controller.findAll.bind(controller))
   }
 
   build (): Router {
