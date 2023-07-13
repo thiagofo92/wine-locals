b8b02146ae746c98f88a399f97c082eaedf194c1
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-13 18:19:44 -0300

    Add middleware helmet and rate limit

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -2,6 +2,7 @@ import Express from 'express'
 import { RoutersServer } from './routers/router'
 import { serve as SwaggerServer, setup as SwaggerSetUp } from 'swagger-ui-express'
 import SwaggerConfig from './middlewares/swagger/swagger-config.json'
+import { helmetConfig, rateLimit } from './middlewares'
 export const app = Express()
 
 const port = process.env.SERVER_PORT || '3000'
@@ -10,6 +11,7 @@ const host = process.env.host || '0.0.0.0'
 export function StartServer (): void {
   const router = new RoutersServer(Express.Router())
   app.use(Express.json())
+  app.use([helmetConfig, rateLimit])
   app.use('/api-docs', SwaggerServer, SwaggerSetUp(SwaggerConfig))
   app.use('/api', router.build())
   app.listen(Number(port), host, () => {
