03b18dc7e05471734eebaecd77080fdc0261b1bb
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-14 11:43:27 -0300

    Change the return type

--- src/app/port/wine-touris.usecase.port.ts
+++ src/app/port/wine-touris.usecase.port.ts
@@ -1,11 +1,11 @@
 
 import { type Either } from '@/shared/errors/either'
-import { type WineTourismAppDto } from '../dto'
+import { type WineTourismAppDtoOutPut, type WineTourismAppDto } from '../dto'
 
 export interface WineTourismUseCasePort {
   create: (input: WineTourismAppDto) => Promise<Either<Error, { id: number }>>
   update: (input: WineTourismAppDto) => Promise<Either<Error, boolean>>
   delete: (id: number) => Promise<Either<Error, boolean>>
-  findById: (id: number) => Promise<Either<Error, WineTourismAppDto>>
-  findAll: () => Promise<Either<Error, WineTourismAppDto[]>>
+  findById: (id: number) => Promise<Either<Error, WineTourismAppDtoOutPut>>
+  findAll: () => Promise<Either<Error, WineTourismAppDtoOutPut[]>>
 }
