349e5d8ea91e06b9265eb5626824512055bec37c
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-14 11:44:29 -0300

    Change the type of open days

--- src/core/entities/wine-tourism.entity.ts
+++ src/core/entities/wine-tourism.entity.ts
@@ -4,20 +4,19 @@ export interface WineTourismEntity {
   name: string
   description: string
   price: string
-  openDays: WineTourismEntity.Week
+  openDays: string
   startHour: string
   endHour: string
   duration: string
 }
-export namespace WineTourismEntity {
-  export enum Week {
-    Monday = 'Monday',
-    Tuesday = 'Tuesday',
-    Wednesday = 'Wednesday',
-    Thursday = 'Thursday',
-    Friday = 'Friday',
-    Saturday = 'Saturday',
-    Sunday = 'Sunday',
-    All = 'All'
-  }
+
+export const Week = {
+  Monday: 'Monday',
+  Tuesday: 'Tuesday',
+  Wednesday: 'Wednesday',
+  Thursday: 'Thursday',
+  Friday: 'Friday',
+  Saturday: 'Saturday',
+  Sunday: 'Sunday',
+  All: 'All'
 }
